6b3f8870c751676908f67fa3c92de51cfdc5ba2f
Tass <tasszz2k@gmail.com>
2025-03-04 18:28:00 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 console.log('Happy developing ✨')
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+}
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
